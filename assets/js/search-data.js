cfa0144f48a31be1a1f46b4391779a82a8d54ab7
qian-chu <97355086+qian-chu@users.noreply.github.com>
2025-12-26 22:19:01 +0000

    Deploying to gh-pages from @ qian-chu/qian-chu.github.io@438a55e4eb39b3e5d36be7e3aa8576f895f58071 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-repositories",
-          title: "Repositories",
-          description: "My GitHub stats and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
